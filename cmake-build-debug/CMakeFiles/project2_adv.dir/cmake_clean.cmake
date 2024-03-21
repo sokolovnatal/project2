f4db86d8fd980d8b402c3218f493fe4402a9b9fc
1109j <jgb5877@rit.edu>
2024-03-21 10:06:12 -0400

    Changes made with Sunny in office hours

---
+++ cmake-build-debug/CMakeFiles/project2_adv.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2_adv.dir/Matrix.cpp.obj"
+  "CMakeFiles/project2_adv.dir/Matrix.cpp.obj.d"
+  "CMakeFiles/project2_adv.dir/main.cpp.obj"
+  "CMakeFiles/project2_adv.dir/main.cpp.obj.d"
+  "libproject2_adv.dll.a"
+  "project2_adv.exe"
+  "project2_adv.exe.manifest"
+  "project2_adv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project2_adv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project2_adv.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for project2_adv.
+# This may be replaced when dependencies are built.
