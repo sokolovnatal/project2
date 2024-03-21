87680541d87f6335436615e6438f10cea97db696
1109j <jgb5877@rit.edu>
2024-03-21 09:59:40 -0400

    Changes made with Sunny in office hours

--- Matrix.cpp
+++ Matrix.cpp
@@ -7,7 +7,7 @@ Matrix::Matrix(int rows, int cols) : rows(rows), cols(cols) {
     // TODO: create a 2D dynamic array (matrix) using the member "data" in Matrix.h
     // TODO: and initialize element values with 0
 
-    auto **data = new double *[rows]; // allocate an array of 3 pointers
+    data = new double *[rows]; // allocate an array of 3 pointers
     for (int i = 0; i < rows; i++) {
         data[i] = new double[cols]; // allocate an array of 3x4 integers
     }
@@ -23,7 +23,7 @@ Matrix::Matrix(int rows, int cols) : rows(rows), cols(cols) {
 Matrix::Matrix(const Matrix& mat) : rows(mat.rows), cols(mat.cols) {
     // TODO: create a 2D dynamic array (matrix) using the member "data" in Matrix.h
     // TODO: and initialize element values with the element values of the parameter mat
-    auto **data = new double *[rows]; // allocate an array of 3 pointers
+    data = new double *[rows]; // allocate an array of 3 pointers
     for (int i = 0; i < rows; i++) {
         data[i] = new double[cols]; // allocate an array of 3x4 integers
     }
@@ -53,6 +53,7 @@ ostream& operator<<(ostream& output, const Matrix& mat) {
           output << mat.data[i][j]; // ask Sunny ?
       }
   }
+  return output;
 }
 
 
@@ -65,6 +66,7 @@ istream& operator>>(istream& input, Matrix& mat) {
             input >> mat.data[i][j]; // ask Sunny ?
         }
     }
+    return input;
 }
 
 Matrix Matrix::operator+(const Matrix& mat) {
@@ -77,9 +79,9 @@ Matrix Matrix::operator+(const Matrix& mat) {
   // Option 1:
   Matrix x = Matrix(rows, cols);
   int row = mat.rows;
-  int cols = mat.cols;
+  int col = mat.cols;
   for(int i = 0; i < row; i++){
-      for(int j = 0; j <cols; j++){
+      for(int j = 0; j <col; j++){
           x.data[i][j] = data[i][j] + mat.data[i][j];
 
       }
@@ -103,9 +105,9 @@ Matrix Matrix::operator-(const Matrix& mat) {
   // TODO: perform matrix subtraction
     Matrix x = Matrix(rows, cols);
     int row = mat.rows;
-    int cols = mat.cols;
+    int col = mat.cols;
     for(int i = 0; i < row; i++){
-        for(int j = 0; j <cols; j++){
+        for(int j = 0; j <col; j++){
             x.data[i][j] = data[i][j]  -  mat.data[i][j];
 
         }
@@ -119,9 +121,9 @@ Matrix& Matrix::operator+=(const Matrix& mat) {
   // TODO: perform matrix addition in place
    // Matrix copy = Matrix() // need to use copy constructor
     int row = mat.rows;
-    int cols = mat.cols;
+    int col = mat.cols;
     for(int i = 0; i < row; i++){
-        for(int j = 0; j <cols; j++){
+        for(int j = 0; j <col; j++){
             data[i][j]= data[i][j]  +  mat.data[i][j];
         }
     }
@@ -136,9 +138,9 @@ Matrix& Matrix::operator-=(const Matrix& mat) {
   assert( (rows == mat.rows) && (cols == mat.cols) );
   // TODO: perform matrix subtraction in place
     int row = mat.rows;
-    int cols = mat.cols;
+    int col = mat.cols;
     for(int i = 0; i < row; i++){
-        for(int j = 0; j <cols; j++){
+        for(int j = 0; j <col; j++){
             data[i][j]= data[i][j]  -  mat.data[i][j];
         }
     }
@@ -150,10 +152,10 @@ Matrix Matrix::operator*(const Matrix& mat) {
   assert( (rows == mat.rows) && (cols == mat.cols) );
   // TODO: perform matrix multiplication
     int row = mat.rows;
-    int cols = mat.cols;
-    int sum = 0;
+    int col = mat.cols;
+    double sum = 0;
     for(int i = 0; i < row; i++){
-        for(int j = 0; j <cols; j++){
+        for(int j = 0; j <col; j++){
            sum += data[i][j]  *  mat.data[i][j];
         }
     }
